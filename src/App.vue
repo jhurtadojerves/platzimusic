249db2cba3e18da8930a639bd56b170846694127
Julio Hurtado <juliens@outlook.com>
2017-12-11 14:44:29 -0500

    Estructurando componentes en vuejs

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,14 @@
     img(src='./assets/logo.png')
     h1 Platzi Music
     ul
-      li(v-for="artist in artists") {{ artist.name }}
-    
+      artist(v-for="artist in artists" v-bind:artist='artist' v-bind:key="artist.mbid")    
 </template>
 
 
 
 
 <script>
-
+import Artist from './components/Artist.vue'
 import getArtists from './api'
 
 export default {
@@ -20,6 +19,9 @@ export default {
     return {
       artists: []
     }
+  },
+  components: {
+    Artist: Artist
   },
   mounted: function () {
     const self = this
