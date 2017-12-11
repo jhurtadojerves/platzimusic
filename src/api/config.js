ed992939b3a50bb784e816852aef4f0b40b77aca
Julio Hurtado <juliens@outlook.com>
2017-12-11 12:41:52 -0500

    Get artist name from API

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default {
+    appName: 'Platzi Music',
+    apiKey: 'f3aecb7ca477a90591c934acc4ac055b',
+    secret: '49fa2a5f5df110366fea76f290e8ff28',
+    registeredTo: 'Julienshurtado'
+}
