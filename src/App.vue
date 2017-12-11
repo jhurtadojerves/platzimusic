ac75d70e5e7b92f9d2b828b309185bfd22648ea3
Julio Hurtado <juliens@outlook.com>
2017-12-11 15:24:50 -0500

    Agregando un spinner a la aplicación

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     h1 Platzi Music
     select(v-model="selectedCountry")
       option(v-for="country in countries" :value="country.value") {{ country.name }}
+    spinner(v-show="loading")
     ul
       artist(v-for="artist in artists" v-bind:artist='artist' v-bind:key="artist.mbid")    
 </template>
@@ -13,6 +14,7 @@
 
 <script>
 import Artist from './components/Artist.vue'
+import Spinner from './components/Spinner.vue'
 import getArtists from './api'
 
 export default {
@@ -26,17 +28,22 @@ export default {
         { name: 'Colombia', value: 'colombia'},
         { name: 'España', value: 'spain'}
       ],
-      selectedCountry: 'ecuador'
+      selectedCountry: 'ecuador',
+      loading: true,
     }
   },
   components: {
-    Artist
+    Artist,
+    Spinner,
   },
   methods: {
     refreshArtists() {
       const self = this
+      this.loading = true
+      this.artists = []
       getArtists(this.selectedCountry)
         .then(function (artists) {
+          self.loading = false
           self.artists = artists
         })
     }
